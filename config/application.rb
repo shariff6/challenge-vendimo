f1b886d9fa29d169afeeecdf0800d6a5cbd6ddf8
Mohammed Shariff <mohammed.shariff@mediapal.net>
2023-03-28 16:00:51 +0300

    Graphql confighs

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,5 @@
 require_relative "boot"
-
+require "sprockets/railtie"
 require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
@@ -10,8 +10,9 @@ module ChallengeVendimo
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
-    # Configuration for the application, engines, and railties goes here.
+    config.session_store :cookie_store, key: "_interslice_session"
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use config.session_store, config.session_options    # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
