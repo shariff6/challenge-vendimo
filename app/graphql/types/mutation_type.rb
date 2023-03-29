31abfce1c4ff82420fc55bab451b37e9a35c8729
Mohammed Shariff <mohammed.shariff@mediapal.net>
2023-03-29 16:25:06 +0300

    Delete valuation mutations

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -5,5 +5,6 @@ module Types
     field :create_furnishing, mutation: Mutations::CreateFurnishing
     field :delete_furnishing, mutation: Mutations::DeleteFurnishing
     field :create_valuation, mutation: Mutations::CreateValuation
+    field :delete_valuation, mutation: Mutations::DeleteValuation
   end
 end
